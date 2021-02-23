6994a6d5ef2b4f75f02f3ebc6dbb819e608a68b3
Artem2021-Step <mistoriwne030405@gmail.com>
2021-02-23 18:59:21 +0200

    testing

---
+++ EFCreateSeeder.DAL/EFContext.cs
@@ -0,0 +1,39 @@
+﻿using EFCreateSeeder.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCreateSeeder.DAL
+{
+    public class EFContext : DbContext
+    {
+        public DbSet<AspNetRole> AspNetRoles { get; set; }
+        public DbSet<AspNetUser> AspNetUsers { get; set; }
+        public DbSet<AspNetUserRoles> UserRoles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=91.238.103.51;Port=5743;Database=allah;Username=gomonmax;Password=578947001;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AspNetUserRoles>(data =>
+            {
+                data.HasKey(primaryKeys => new { primaryKeys.RoleId, primaryKeys.UserId });
+
+                data.HasOne(userRolesVirtualColumnFromRole => userRolesVirtualColumnFromRole.Role)
+                .WithMany(userRolesVirtualCollectionFromUserRoles => userRolesVirtualCollectionFromUserRoles.UserRoles)
+                .HasForeignKey(userRolesIntColumnFromRoleWithForeignSettings => 
+                userRolesIntColumnFromRoleWithForeignSettings.RoleId)
+                .IsRequired();
+                data.HasOne(userRolesVirtualColumnFromUser => userRolesVirtualColumnFromUser.User)
+                .WithMany(userRolesVirtualCollectionFromUserRoles => userRolesVirtualCollectionFromUserRoles.UserRoles)
+                .HasForeignKey(userRolesIntColumnWithForeignSettings => userRolesIntColumnWithForeignSettings.UserId)
+                .IsRequired();
+                
+            });
+        }
+    }
+}
---
+++ EFCreateSeeder.DAL/Services/DBSeeder.cs
@@ -0,0 +1,88 @@
+﻿using EFCreateSeeder.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EFCreateSeeder.DAL.Services
+{
+    public static class DBSeeder
+    {
+        public static void SeedAll(EFContext context) 
+        {
+            SeedRoles(context);
+            SeedUsers(context);
+            SeedUserRoles(context);
+        }
+        private static void SeedRoles(EFContext context) 
+        {
+            if (context.AspNetRoles.Count() == 0) 
+            {
+                context.AspNetRoles.AddRange(new List<AspNetRole> { 
+                new AspNetRole
+                {
+                    Name = "Doctor",
+                    NormalizedName = "Doctor Cardiolog",
+                    ConcurrencyStamp = ""
+                },
+                new AspNetRole
+                {
+                    Name = "Teacher",
+                    NormalizedName = "Web-Design Teacher",
+                    ConcurrencyStamp = ""
+                },
+                new AspNetRole
+                {
+                    Name = "Manager",
+                    NormalizedName = "Manger for Sales",
+                    ConcurrencyStamp = ""
+                }
+                });
+
+                context.SaveChanges();
+            }
+        }
+        private static void SeedUsers(EFContext context) 
+        {
+            if (context.AspNetUsers.Count() == 0) 
+            {
+                context.AspNetUsers.AddRange(new List<AspNetUser> {
+                new AspNetUser
+                {
+                    UserName = "Nazar",
+                    TwoFactorEnabled = true,
+                    AccessFailedCount = "0",
+                    ConcurrencyStamp = "",
+                    EmailConfirmed = true,
+                    Email = "loh.govno.sobaka@lashara.com",
+                    LockoutEnabled = true,
+                    LockoutEnd =  DateTime.Now,
+                    NormalizedEmail = "loh.govno.sobaka@lashara.com",
+                    NormalizedUserName ="Nazar Politaew",
+                    PasswordHash = "333222111",
+                    PhoneNumber = "069696969696969696969",
+                    PhoneNumberConfirmed = true,
+                    SecurityStamp = ""
+                }
+                });
+
+                context.SaveChanges();
+            }
+        }
+        private static void SeedUserRoles(EFContext context) 
+        {
+            if (context.UserRoles.Count() == 0) 
+            {
+                context.AddRange(new List<AspNetUserRoles> { 
+                new AspNetUserRoles 
+                {
+                    UserId = 1,
+                    RoleId = 1,
+                }
+                });
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
